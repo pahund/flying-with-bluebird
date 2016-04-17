961ba773e1e3e0369376092063f7e5f3e42129d9
Patrick Hund <pahund@team.mobile.de>
2016-04-17 16:49:31 +0200

    initial commit with coroutine example from Bluebird docs
    Change-Id: I21ff96f1433cfe170f2e1abf95a8d51d0c27841b

---
+++ src/coroutine.js
@@ -0,0 +1,27 @@
+/**
+ * coroutine.js
+ *
+ * @author <a href="https://github.com/pahund">Patrick Hund</a>
+ * @since 17 Apr 2016
+ */
+const Promise = require("bluebird");
+
+function PingPong() {
+
+}
+
+PingPong.prototype.ping = Promise.coroutine(function* (val) {
+    console.log("Ping?", val);
+    yield Promise.delay(500);
+    this.pong(val+1);
+});
+
+PingPong.prototype.pong = Promise.coroutine(function* (val) {
+    console.log("Pong!", val);
+    yield Promise.delay(500);
+    this.ping(val+1);
+});
+
+var a = new PingPong();
+a.ping(0);
+
